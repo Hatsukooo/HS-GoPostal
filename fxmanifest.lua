c36822508f31d2bc17eb62e7827f4bf529cb8194
Hatsuko <quersondc@gmail.com>
2023-05-14 19:49:18 +0200

    0 changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
+Author 'Hatusko#0818'
+Description 'Simple delivery script,  Working Duty, should not be glitchy and should work just fine
+
 client_scripts {
     'config.lua',
     'client/*.lua',
